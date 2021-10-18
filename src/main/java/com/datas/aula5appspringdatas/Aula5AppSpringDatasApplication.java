f0990b0e0c4fed480d6ca2f2727da8fbc373577d
ramonr07 <r>
2021-10-18 11:43:16 -0300

    AppDate

---
+++ src/main/java/com/datas/aula5appspringdatas/Aula5AppSpringDatasApplication.java
@@ -0,0 +1,14 @@
+package com.datas.aula5appspringdatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Aula5AppSpringDatasApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula5AppSpringDatasApplication.class, args);
+    }
+
+}
